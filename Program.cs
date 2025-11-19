5338ac96fb95a7d27841a4a0a255a8da3b01fb72
Nhat <105680164@student.swin.edu.au>
2025-11-19 18:57:30 +0700

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CustomProgram;
+using System;
+using System.Linq;
+using System.Windows;
+using SplashKitSDK;
+
+
+namespace MainProgram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Window window = new Window("Flappy Bird", 800, 600);
+            GameManager gameManager = new GameManager();
+            
+            while (!window.CloseRequested)
+            {
+                SplashKit.ProcessEvents();
+                
+                // Update game with proper delta time
+                gameManager.UpdateGame(SplashKit.TimerTicks("frameTimer") / 1000.0f);
+                SplashKit.ResetTimer("frameTimer");
+            }
+        }
+    }
+}
